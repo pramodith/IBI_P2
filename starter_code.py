fd745b1e601ae4d8f3c1c8d225f76530315506a0
Tanmay Binaykiya <btanmay@gatech.edu>
2018-04-11 14:15:11 -0400

    [Tanmay] * Part 6 :FullBody ; FFT + PCA + GradBoosting. F-Score: 0.60, * Uploading all models * Results added

--- starter_code.py
+++ starter_code.py
@@ -4,31 +4,74 @@ import numpy as np
 from scipy.stats import mode
 from part4_classifier import Part4Classifier
 from sklearn.externals import joblib
+from scipy.fftpack import fftfreq, rfft
+from sklearn.decomposition import PCA
 
 
-# import imu_wrist_p3
-
-def create_io_pairs(inputs, labels, window_size, stride, feature_extraction_method):
+def create_io_pairs(inputs, labels, window_size, stride, feature_extraction_method, post_processor):
     """
     [STUDENT] Computes your windowed features here and labels
     :param inputs:
     :param labels:
     :param window_size:
     :param stride:
+    :param feature_extraction_method: lambda/function to extract features from x
+    :param post_processor: lambda/function post_processor to apply to each feature
     :return:
     """
+    print("Creating io pairs...")
+    if isinstance(feature_extraction_method, str) and feature_extraction_method == "FFT_REL_POW":
+        io_pairs = create_io_pairs_fft(inputs, labels, window_size, stride)
+    else:
+        X = inputs
+        Y = labels
+        final_x = []
+        final_y = []
+        for i in range(0, len(X), stride):
+            if i + window_size < len(X):
+                inp_x = X[i:i + window_size]
+                inp_x = feature_extraction_method(inp_x)  # np.mean(inp_x, axis=0)
+                out_y = int(np.asscalar(mode(Y[i:i + window_size])[0][0]))
+                final_x.append(inp_x)
+                final_y.append(out_y)
+        io_pairs = final_x, final_y
+    print("Done")
+    if post_processor:
+        print("Post Processing...")
+        return post_processor(io_pairs[0]), io_pairs[1]
+
+
+def create_io_pairs_fft(inputs, labels, window_size, stride):
+    # Compute your windowed features here and labels. Right now
+    # it just returns the inputs and labels without changing anything.
+    cnt = 0
     X = inputs
     Y = labels
     final_x = []
     final_y = []
+    print("FFT. ", len(X))
     for i in range(0, len(X), stride):
+        if i % 100 == 0:
+            print("FFT i:", i)
         if i + window_size < len(X):
             inp_x = X[i:i + window_size]
-            inp_x = feature_extraction_method(inp_x)  # np.mean(inp_x, axis=0)
+            # inp_x = np.mean(inp_x,axis=0)
+            # inp_x = np.std(inp_x,axis=0)
+            inp_x = np.abs(rfft(inp_x, axis=0))
+            if np.count_nonzero(np.sum(inp_x, axis=0) == 0) > 0:
+                continue
+            inp_x /= np.sum(inp_x, axis=0)
+            final_x.append([])
+            tr_inp_x = inp_x.T
+            for r in tr_inp_x:
+                coeff = fftfreq(r.shape[0])
+                hist, bins = np.histogram(coeff, weights=r, bins=5)
+                final_x[cnt].extend(hist)
+            cnt += 1
             out_y = int(np.asscalar(mode(Y[i:i + window_size])[0][0]))
-            final_x.append(inp_x)
             final_y.append(out_y)
-    return final_x, final_y
+
+    return np.asarray(final_x), final_y
 
 
 def impute_data(arr):
@@ -91,10 +134,22 @@ def test(X, model):
     return model["clf"].predict(X)
 
 
-def cv_train_test(dataset, sensors, labels, window, stride, model_file_name, feature_extraction_method):
+def cv_train_test(dataset, sensors, labels, window, stride, model_file_name, feature_extraction_method,
+                  post_processor):
     """
     Template code for performing leave on subject out cross-validation
+
+    :param dataset:
+    :param sensors:
+    :param labels:
+    :param window:
+    :param stride:
+    :param model_file_name:
+    :param feature_extraction_method:
+    :param post_processor:
+    :return:
     """
+
     subjects = dataset.subject_data.keys()
     Y_pred_total, Y_test_total = [], []
 
@@ -111,10 +166,12 @@ def cv_train_test(dataset, sensors, labels, window, stride, model_file_name, fea
         X_test = impute_data(X_test)
         # Compute features and labels for train and test set
         X_train, Y_train = create_io_pairs(X_train, Y_train, window, stride,
-                                           feature_extraction_method=feature_extraction_method)
+                                           feature_extraction_method=feature_extraction_method,
+                                           post_processor=post_processor)
         # print(Counter(Y_train))
         X_test, Y_test = create_io_pairs(X_test, Y_test, window, stride,
-                                         feature_extraction_method=feature_extraction_method)
+                                         feature_extraction_method=feature_extraction_method,
+                                         post_processor=post_processor)
         # print(Counter(Y_test))
         Y_test = np.asarray(Y_test)
         model = train(X_train, Y_train)
@@ -131,22 +188,29 @@ def cv_train_test(dataset, sensors, labels, window, stride, model_file_name, fea
     eval_preds(Y_pred_total, Y_test_total, labels["classes"])
 
 
-def custom_run(dataset, is_locomotion, sensor_data_field, window_size, stride, fem_method="MEAN"):
+def custom_run(dataset, is_locomotion, sensor_data_field, window_size, stride, post_processor, fem_method="MEAN"):
     """
-    Runs one instance of training
+    Runs one instance of training/test
+
+    :param dataset:
+    :param is_locomotion:
+    :param sensor_data_field:
+    :param window_size:
+    :param stride:
+    :param post_processor:
+    :param fem_method:
     :return:
     """
-
     SENSORS = ["AccelWristSensors", "ImuWristSensors", "FullBodySensors"]
     assert sensor_data_field in SENSORS
     print("Sensor: ", sensor_data_field)
 
     fem_methods = {
         "MEAN": lambda x: np.mean(x, axis=0),
-        "RMS": lambda x: np.linalg.norm(x, axis=0)/np.sqrt(window_size),
+        "RMS": lambda x: np.linalg.norm(x, axis=0) / np.sqrt(window_size),
         "STD_DEV": lambda x: np.std(x, axis=0),
         "MEAN_ABS_DEV": lambda x: NotImplementedError,
-        "FFT_REL_POW": lambda x: np.fft.fft(x, axis=0)
+        "FFT_REL_POW": "FFT_REL_POW"
     }
     assert fem_method in fem_methods.keys()
     print("Feature Extraction Method: ", fem_method)
@@ -162,21 +226,29 @@ def custom_run(dataset, is_locomotion, sensor_data_field, window_size, stride, f
 
     cv_train_test(dataset, sensors, labels, window_size, stride,
                   model_file_name="{}-W{}-S{}".format(sensor_data_field, window_size, stride),
-                  feature_extraction_method=fem_methods[fem_method])
+                  feature_extraction_method=fem_methods[fem_method], post_processor=post_processor)
 
 
-def window_stride_generator(gen_pairs=True):
-    window_sizes = [5, 10, 15]
-    strides = [2, 3, 5]
+def window_stride_generator(gen_pairs=True, window_sizes=[5, 10, 15], strides=[2, 3, 5]):
     pairs = zip(window_sizes, strides)
-
     if gen_pairs:
         for pair in pairs:
             yield pair[0], pair[1]
     else:
-        for w_size in window_sizes:
-            for stryd in strides:
-                yield w_size, stryd
+        for window_size in window_sizes:
+            for stride in strides:
+                yield window_size, stride
+
+
+def pca_post_processor(X, components=20, iterated_power=50):
+    """
+    :param X:
+    :param components:
+    :return:
+    """
+    pca = PCA(n_components=components, whiten=True, iterated_power=iterated_power, copy=False)
+    print("PCA:", pca)
+    return pca.fit_transform(np.array(X))
 
 
 def main_method():
@@ -185,14 +257,25 @@ def main_method():
     print("Done...")
     print("Computing Imputation error...")
     imputation_diff = test_imputation(dataset)
-    print("Imputation error: {} \n".format(imputation_diff))
+    assert imputation_diff == 0, "Imputation error: {}".format(imputation_diff)
+    print("Imputation error: 0")
 
-    for w, s in window_stride_generator():
-        custom_run(dataset, False, "FullBodySensors", w, s, "RMS")
+    for w, s in window_stride_generator(window_sizes=[5], strides=[2]):
+        custom_run(dataset=dataset, is_locomotion=False, sensor_data_field="FullBodySensors", window_size=w, stride=s,
+                   post_processor=pca_post_processor, fem_method="FFT_REL_POW")
+
+
+def test_pca():
+    np.random.seed(1)
+    X = np.array([[-1, 2, -2], [-2, 8, -4], [-3, 18, -6], [1, 2, 2], [2, 8, 4], [3, 18, 9]])
+    print("pca result: ", pca_post_processor(X, components=2, iterated_power=4))
+    print("pca result: ", pca_post_processor(X, components=1, iterated_power=4))
+
+    X = np.array([[-1, 2], [-2, 8], [-3, 18], [1, 2], [2, 8], [3, 18]])
+    print("pca result: ", pca_post_processor(X, components=2, iterated_power=4))
+    print("pca result: ", pca_post_processor(X, components=1, iterated_power=4))
 
 
 if __name__ == "__main__":
     main_method()
-
-# Activity labels
-# cv_train_test(dataset, sensors, dataset.activity_labels)
+    # test_pca()
